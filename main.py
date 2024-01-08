10a843b7b2b7ea4d928b749f9a502f7a686f450b
poisonIvy321 <155780235+poisonIvy321@users.noreply.github.com>
2024-01-08 22:07:13 +0000

    Added dialogue and asked user their name/age
    Added more to the chatbox

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print('Welcome to the Elite 101 Chatbox!!')
-input('Please Enter Your name:  ')
+name = input('Please Enter Your name:  ')
+
+print(f'Hello, {name}!!')
+age = input('How old are you??  ')
+
+print(f'Wow, {age} is a great age to be!! \nHow can I help you today?')
+
